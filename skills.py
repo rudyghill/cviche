9c8358aa30fb7e3988c01ffdc50fe83d4ebbec02
Rudy Hill <rudyghill@gmail.com>
2024-02-25 07:45:54 -0700

    formatting with black

--- skills.py
+++ skills.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def skills(filename, tag):
     master = []
     selected = []
@@ -7,14 +8,15 @@ def skills(filename, tag):
         for line in fin:
             skill, *tags = line.strip().split(",")
             skill = skill.strip()
-            tags = list(map(lambda x:x.strip().lower(),tags))
+            tags = list(map(lambda x: x.strip().lower(), tags))
             master.append(dict(skill=skill, tags=tags))
             if tag in tags:
                 selected.append(skill)
     return master, selected
 
+
 # TODO: safegaurd against no filename
-if __name__ == '__main__':
+if __name__ == "__main__":
     master, selected = skills(sys.argv[1], sys.argv[2])
     print(*selected, sep=",")
 # import json
--- skills_test.py
+++ skills_test.py
@@ -3,13 +3,15 @@ import unittest
 import unittest.mock
 from skills import skills
 
-@unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+
+@unittest.mock.patch("sys.stdout", new_callable=io.StringIO)
 class TestSkills(unittest.TestCase):
 
     def test_spacing(self, mock_stdout):
-        master, selected = skills("example.csv","libre")
+        master, selected = skills("example.csv", "libre")
         print(*selected, sep=",")
-        self.assertEqual(mock_stdout.getvalue(),"Writer,Calc,Impress\n")
+        self.assertEqual(mock_stdout.getvalue(), "Writer,Calc,Impress\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
