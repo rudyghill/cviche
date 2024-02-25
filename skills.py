e3eb21ac2c8facb59d53a6c0c6664cae61dce85f
Rudy Hill <rudyghill@gmail.com>
2024-02-25 00:16:01 -0700

    formatted code with black

--- skills.py
+++ skills.py
@@ -1,17 +1,17 @@
-#initialize json
-#TODO: normalize input including case
-#TODO: create an example.csv
+# initialize json
+# TODO: normalize input including case
+# TODO: create an example.csv
 master = []
 with open("skills.csv", "r") as fin:
     for line in fin:
-        skill, score, *tags = line.replace(' ','').strip().split(",")
+        skill, score, *tags = line.replace(" ", "").strip().split(",")
         master.append(dict(skill=skill, score=int(score), tags=tags))
 
-#import json
-#print(json.dumps(master)) #can dump into a json file later
+# import json
+# print(json.dumps(master)) #can dump into a json file later
 
-#print skills with certain tags
-tag="security"
+# print skills with certain tags
+tag = "security"
 for item in master:
     if tag in item["tags"]:
         print(item["skill"])
