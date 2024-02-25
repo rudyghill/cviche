9b34f0d79500b10c453d806bcb6cc5f520d56cf4
Rudy Hill <rudyghill@gmail.com>
2024-02-25 07:50:38 -0700

    added more unit tests

--- skills_test.py
+++ skills_test.py
@@ -12,6 +12,16 @@ class TestSkills(unittest.TestCase):
         print(*selected, sep=",")
         self.assertEqual(mock_stdout.getvalue(), "Writer,Calc,Impress\n")
 
+    def test_case(self, mock_stdout):
+        master, selected = skills("example.csv", "microsoft")
+        print(*selected, sep=",")
+        self.assertEqual(mock_stdout.getvalue(), "Word,Excel,Powerpoint\n")
+
+    def test_case_and_spacing(self, mock_stdout):
+        master, selected = skills("example.csv", "apple")
+        print(*selected, sep=",")
+        self.assertEqual(mock_stdout.getvalue(), "Pages,Numbers,Keynote\n")
+
 
 if __name__ == "__main__":
     unittest.main()
