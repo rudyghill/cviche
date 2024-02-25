6de74d2b6a76c1d372a80b462e7456ecd98acdee
Rudy Hill <rudyghill@gmail.com>
2024-02-25 05:45:28 -0700

    refactoring with function

--- skills.py
+++ skills.py
@@ -1,19 +1,20 @@
-# initialize json
-master = []
-selected = []
 import sys
 
+def skills(filename, tag):
+    master = []
+    selected = []
+    with open(filename, "r") as fin:
+        for line in fin:
+            skill, *tags = line.strip().split(",")
+            skill = skill.strip()
+            tags = list(map(lambda x:x.strip().lower(),tags))
+            master.append(dict(skill=skill, tags=tags))
+            if tag in tags:
+                selected.append(skill)
+    return master, selected
+
 # TODO: safegaurd against no filename
-filename = sys.argv[1]
-tag = sys.argv[2]
-with open(filename, "r") as fin:
-    for line in fin:
-        skill, *tags = line.strip().split(",")
-        skill = skill.strip()
-        tags = list(map(lambda x:x.strip().lower(),tags))
-        master.append(dict(skill=skill, tags=tags))
-        if tag in tags:
-            selected.append(skill)
+master, selected = skills(sys.argv[1], sys.argv[2])
 
 print(*selected, sep=",")
 # import json
