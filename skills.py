85133169cb928c2cc775f041895d0f78018fcb76
Rudy Hill <rudyghill@gmail.com>
2024-03-06 15:37:37 -0700

    streamlined logic

--- skills.py
+++ skills.py
@@ -7,8 +7,8 @@ def skills_csv(filename, tag):
     with open(filename, "r") as fin:
         csv_reader = csv.DictReader(fin, delimiter=",")
         for line in csv_reader:
-            tags = line["tags"].strip().lower().split(",")
-            line["tags"] = list(map(lambda x: x.strip(), tags))
+            tags = line["tags"].split(",")
+            line["tags"] = list(map(lambda x: x.strip().lower(), tags))
             if tag in line["tags"]:
                 selected.append(line["skill"])
     return csv_reader, selected
