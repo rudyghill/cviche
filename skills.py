670cb072bd6b908466a849d90bbff0fed00c2f2f
Rudy Hill <rudyghill@gmail.com>
2024-03-06 15:34:45 -0700

    rewrote skills with csv library -direct dictionary read -headers dictate dict fields -tags now all in one field

--- skills.py
+++ skills.py
@@ -1,18 +1,21 @@
 import sys
+import csv
+
+
+def skills_csv(filename, tag):
+    selected = []
+    with open(filename, "r") as fin:
+        csv_reader = csv.DictReader(fin, delimiter=",")
+        for line in csv_reader:
+            tags = line["tags"].strip().lower().split(",")
+            line["tags"] = list(map(lambda x: x.strip(), tags))
+            if tag in line["tags"]:
+                selected.append(line["skill"])
+    return csv_reader, selected
 
 
 def skills(filename, tag):
-    master = {}
-    selected = []
-    with open(filename, "r") as fin:
-        for line in fin:
-            skill, *tags = line.strip().split(",")
-            skill = skill.strip()
-            tags = list(map(lambda x: x.strip().lower(), tags))
-            master[skill] = tags
-            if tag in tags:
-                selected.append(skill)
-    return master, selected
+    return skills_csv(filename, tag)
 
 
 # TODO: safegaurd against no filename
