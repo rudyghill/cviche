98f1396c4213f66249d0cb65f35ee6ef54e427f1
Rudy Hill <rudyghill@gmail.com>
2024-03-19 01:04:56 -0600

    removed skills function

--- skills.py
+++ skills.py
@@ -2,18 +2,10 @@ import sys
 import utilities
 
 
-# TODO: delete function
-def skills(filename, tag):
-    master = utilities.csv_to_table(filename)
-    selected = utilities.select_values_with_tag(master, "skill", "tags", tag)
-    return master, selected
-
-
 # TODO: safegaurd against no filename
 if __name__ == "__main__":
     master = utilities.csv_to_table(sys.argv[1])
     selected = utilities.select_values_with_tag(master, "skill", "tags", sys.argv[2])
-    # master, selected = skills(sys.argv[1], sys.argv[2])
     print(*selected, sep=", ")
 # import json
 # print(json.dumps(master)) #can dump into a json file later
